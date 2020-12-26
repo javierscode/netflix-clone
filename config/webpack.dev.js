9774b150f361f6fb7387e90b984a847692f53c6c
soyjavierlinares <javihp79@gmail.com>
2020-12-26 11:10:29 +0100

    webpack-dev-server configuration

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -8,6 +8,6 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: path.join(__dirname, 'public'),
+    contentBase: path.join(__dirname, 'dist'),
   },
 });
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { render } from "react-dom";
 function App() {
     const [state, setState] = useState("CLICK ME");
 
-    return (<button onClick={() => setState("CLICKED")}>{state}</button>);
+    return (<button onClick={() => setState("hola")}>{state}</button>);
 }
 
 render(<App />, document.getElementById("root"));
