780dab533280c690ad90d5a06c7c05f6d9b30103
soyjavierlinares <javihp79@gmail.com>
2020-12-26 12:00:09 +0100

    Added globals css

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import { render } from "react-dom";
 function App() {
     const [state, setState] = useState("CLICK ME");
 
-    return (<button onClick={() => setState("hola")}>{state}</button>);
+    return (<> 
+    <h1>Hola esto es una prueba</h1>
+    <button onClick={() => setState("hola")}>{state}</button></>);
 }
 
 render(<App />, document.getElementById("root"));
