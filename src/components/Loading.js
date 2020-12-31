8d34eab845eb795fba600dce9f22bb40b7179d7a
soyjavierlinares <javihp79@gmail.com>
2020-12-31 12:34:41 +0100

    New Loading component

---
+++ src/components/Loading.js
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+const Loading =()=>{
+
+
+    return (
+        <div className="loading">
+            <p>Loading</p>
+        </div>
+    )
+}
+
+export default Loading;
