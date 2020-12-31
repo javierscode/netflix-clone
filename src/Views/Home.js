8d1fee3436742b08efabce040646093d480f14e8
soyjavierlinares <javihp79@gmail.com>
2020-12-31 11:39:53 +0100

    Getting context value in Home view

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useMovies } from '../context/MoviesContext'
+import { useShows } from '../context/ShowsContext';
 
 export default function Home() {
 
-    
+    const movies = useMovies()
+    const shows= useShows()
+
+    console.log(movies);
+    console.log(shows);
+
+
     return (
         <div>
             Home
