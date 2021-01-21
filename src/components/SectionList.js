8d6430847d233b8c5edebdb57dcbb7fe936fe7d8
soyjavierlinares <javihp79@gmail.com>
2021-01-21 15:26:41 +0100

    Added Arrow to SectionList Component

--- src/components/SectionList.js
+++ src/components/SectionList.js
@@ -1,18 +1,57 @@
-import React from 'react'
-import { getOriginalImage } from '../utils'
+import React, { useEffect, useRef, useState } from "react";
+import { getOriginalImage } from "../utils";
+import Arrow from "./icons/Arrow";
 
-const SectionList = ({title, list}) => {
-    return (
-        <div className="section-list">
-            <h2>{title}</h2>
-            <div className="list">
-                {list.map((item)=>(
-                  <div className="item" style={{backgroundImage: "url('"+getOriginalImage(item.backdrop_path)+"')"}}>
-                    <h3>{item.name ? item.name : item.title}</h3>
-                  </div>
-                ))}
-            </div>
+const SectionList = ({ title, list }) => {
+
+  const listRef = useRef(null);
+  const leftRef = useRef(null);
+
+  const MoveTo =(type)=>{
+
+    const moves ={
+      "left": -360,
+      "right": +360
+    };
+
+    let scroll = listRef.current.scrollLeft + moves[type]
+
+    listRef.current.scrollTo({
+      left: scroll,
+      behavior: 'smooth'
+    });
+
+    scroll <= 0 ? leftRef.current.style.opacity = "0" : leftRef.current.style.opacity = "1";
+
+  }
+
+  useEffect(() => {
+    listRef.current.scrollLeft <= 0 ? leftRef.current.style.opacity = "0" : leftRef.current.style.opacity = "1";
+  }, [])
+
+  return (
+    <div className="section-list">
+      <h2>{title}</h2>
+      <div className="controls">
+        <div ref={leftRef} onClick={()=> MoveTo('left')}><Arrow fill="#ffffff" height="30px"/></div>
+        <div onClick={()=> MoveTo('right')}><Arrow fill="#ffffff" height="30px" transform="rotate(180)"/></div>
+      </div>
+      <div className="list" ref={listRef}>
+        {list.map((item) => (
+          <div
+            className="item"
+            style={{
+              backgroundImage:
+                "url('" + getOriginalImage(item.backdrop_path) + "')",
+            }}
+          >
+            <h3>{item.name ? item.name : item.title}</h3>
           </div>
-    )
-}
-export default SectionList
+        ))}
+      </div>
+
+    </div>
+  );
+  
+};
+export default SectionList;
