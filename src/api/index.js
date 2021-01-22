9f05195eff711a0d5001606e043aaf3360fea557
soyjavierlinares <javihp79@gmail.com>
2021-01-22 11:28:24 +0100

    Get movies and tv shows by genre

--- src/api/index.js
+++ src/api/index.js
@@ -1,74 +1,79 @@
-import {useState} from 'react'
-import { getRandomElementFromArray } from '../utils';
-import { API_KEY, API_URL } from './config';
+import { useState } from "react";
+import { getRandomElementFromArray } from "../utils";
+import { API_KEY, API_URL } from "./config";
 
-export const useAPI = () =>{
+export const useAPI = () => {
+  const [state, setState] = useState({ loading: false, error: false });
 
-    const [state, setState] = useState({ loading: false, error: false })
+  const getData = async (endpoint, page = 1, extraParameters = "") => {
+    setState({ loading: true, error: false });
 
+    try {
+      const result = await fetch(
+        API_URL + endpoint + API_KEY + "&page=" + page + extraParameters
+      );
+      const data = await result.json();
 
-    const getData= async(endpoint, page=1, extraParameters="") =>{
-
-        setState({loading:true, error:false});
-
-        try {
-            
-            const result = await fetch(API_URL+endpoint+API_KEY+"&page="+page+extraParameters);
-            const data = await result.json();
-
-            setState({ loading: false, error: false} );
-            return data;
-            
-        } catch (error) {
-            setState({loading:false, error});
-        }
-
+      setState({ loading: false, error: false });
+      return data;
+    } catch (error) {
+      setState({ loading: false, error });
     }
+  };
 
-    const getMostPopularMovies = async()=>{
-        return await getData('/movie/popular')
-    }
+  const getMostPopularMovies = async () => {
+    return await getData("/movie/popular");
+  };
 
-    const getMostPopularTVShows = async()=>{
-        return await getData('/tv/popular')
-    }
+  const getMostPopularTVShows = async () => {
+    return await getData("/tv/popular");
+  };
 
-    const getTopRatedMovies = async()=>{
-        return await getData('/movie/top_rated')
-    }
+  const getTopRatedMovies = async () => {
+    return await getData("/movie/top_rated");
+  };
 
-    const getTopRatedTVShows = async()=>{
-        return await getData('/tv/top_rated')
-    }
+  const getTopRatedTVShows = async () => {
+    return await getData("/tv/top_rated");
+  };
 
+  const getVideosFromMovie = async (id) => {
+    const endpoint = `/movie/${id}/videos`;
+    return await getData(endpoint);
+  };
 
-    const getVideosFromMovie = async(id)=>{
+  const getVideosFromTVShow = async (id) => {
+    const endpoint = `/tv/${id}/videos`;
+    return await getData(endpoint);
+  };
 
-        const endpoint = `/movie/${id}/videos`
-        return await getData(endpoint);
+  const getNetflixOriginalsMovies = async () => {
+    return await getData("/movie/popular", 1, "&with_networks=213");
+  };
 
-    }
+  const getNetflixOriginalsTVShows = async () => {
+    return await getData("/tv/popular", 1, "&with_networks=213");
+  };
 
-    const getVideosFromTVShow = async(id)=>{
+  const getMoviesByGenre = async (id) =>{
+    return await getData("/movie/popular", 1,`&with_genre=${id}`);
+  };
 
-        const endpoint = `/tv/${id}/videos`
-        return await getData(endpoint);
+  const getTVShowsByGenre = async (id) =>{
+    return await getData("/tv/popular", 1,`&with_genre=${id}`);
+  };
 
-    }
-
-    const getNetflixOriginalsMovies = async()=>{
-
-        return await getData('/movie/popular',1,'&with_networks=213');
-
-    }
-
-    const getNetflixOriginalsTVShows= async()=>{
-
-        return await getData('/tv/popular',1,'&with_networks=213');
-
-    }
-    
-
-    return {...state, getMostPopularMovies, getMostPopularTVShows, getVideosFromMovie,getVideosFromTVShow, getTopRatedMovies, getTopRatedTVShows, getNetflixOriginalsMovies, getNetflixOriginalsTVShows}
-
-}
+  return {
+    ...state,
+    getMostPopularMovies,
+    getMostPopularTVShows,
+    getVideosFromMovie,
+    getVideosFromTVShow,
+    getTopRatedMovies,
+    getTopRatedTVShows,
+    getNetflixOriginalsMovies,
+    getNetflixOriginalsTVShows,
+    getMoviesByGenre,
+    getTVShowsByGenre
+  };
+};
